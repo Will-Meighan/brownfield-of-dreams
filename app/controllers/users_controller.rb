ae9399175afa6c7b0c001de887776009cbfd0477
Kathleen Carroll <khakicarroll@comcast.net>
2020-03-27 10:14:55 -0600

    Finish repositories, followers, and following stories

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,35 @@
 class UsersController < ApplicationController
   def show
     github = GithubService.new
-    raw = github.user_info(current_user)
+    raw_repos = github.user_info(current_user, 'repos')
+
+    if raw_repos.count == 2
+      @repos = nil
+    else
+      @repos = raw_repos.map do |row|
+        Repository.create(url: row[:owner][:html_url], name: row[:name])
+      end
+    end
+
+    raw_followers = github.user_info(current_user, 'followers')
+
+    if raw_followers.count == 2 || raw_followers == []
+      @followers = nil
+    else
+      @followers = raw_followers.map do |row|
+        Follower.create(url: row[:html_url], name: row[:login])
+      end
+    end
+
+    raw_following = github.user_info(current_user, 'following')
+
+    if raw_following.count == 2 || raw_following == []
+      @following = nil
+    else
+      @following = raw_following.map do |row|
+        Following.create(url: row[:html_url], name: row[:login])
+      end
+    end
   end
 
   def new
