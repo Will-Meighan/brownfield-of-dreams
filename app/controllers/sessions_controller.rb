4e6aa52aef2d63f9d1ddb9b2b85027e932cb838a
Kathleen Carroll <khakicarroll@comcast.net>
2020-03-28 15:21:05 -0600

    Finish initial oauth task

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,35 @@ class SessionsController < ApplicationController
     end
   end
 
+  def github
+    client_id     = ENV['GITHUB_CLIENT_ID']
+    client_secret = ENV['GITHUB_CLIENT_SECRET']
+    code          = params[:code]
+    response      = Faraday.post("https://github.com/login/oauth/access_token?client_id=#{client_id}&client_secret=#{client_secret}&code=#{code}")
+
+    pairs = response.body.split("&")
+    response_hash = {}
+    pairs.each do |pair|
+      key, value = pair.split("=")
+      response_hash[key] = value
+    end
+
+    token = response_hash["access_token"]
+
+    oauth_response = Faraday.get("https://api.github.com/user?access_token=#{token}")
+
+    auth = JSON.parse(oauth_response.body)
+
+    user = current_user
+    user.update_attribute(:username, auth["login"])
+    user.update_attribute(:uid, auth["id"])
+    user.update_attribute(:token, token)
+
+    session[:user_id] = user.id
+
+    redirect_to dashboard_path
+  end
+
   def destroy
     session[:user_id] = nil
     redirect_to root_path
