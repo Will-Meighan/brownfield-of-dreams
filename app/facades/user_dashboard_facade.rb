a8e3c57f03f77a31abbe213896b33a929796e21b
William Meighan <williamdmeighan@gmail.com>
2020-03-27 18:46:41 -0600

    create dashboard facade to generate followers of the current_user

---
+++ app/facades/user_dashboard_facade.rb
@@ -0,0 +1,36 @@
+class UserDashboardFacade
+  attr_reader :token
+
+  def initialize(user)
+    @user = user
+    @token = user.github_token
+  end
+
+  def repos
+    return @repos if @repos
+
+    service = GithubService.new(@token)
+    @repos = service.get_repos.map do |repo|
+      Repository.new(repo)
+    end
+  end
+
+  def followers
+    return @followers if @followers
+
+    service = GithubService.new(@token)
+    @followers = service.get_followers.map do |follower|
+      Follower.new(follower)
+    end
+  end
+
+  def following
+    return @following if @following
+
+    service = GithubService.new(@token)
+    @following = service.get_following.map do |following|
+      Following.new(following)
+    end
+  end
+
+end
