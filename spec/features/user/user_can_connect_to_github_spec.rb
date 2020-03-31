44921d505f833b50df499261b39e4cae150171ec
Kathleen Carroll <khakicarroll@comcast.net>
2020-03-31 10:26:37 -0600

    Finish testing omniauth

--- spec/features/user/user_can_connect_to_github_spec.rb
+++ spec/features/user/user_can_connect_to_github_spec.rb
@@ -6,30 +6,26 @@ describe 'OAuth' do
     Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:github]
   end
 
-  xit "should successfully create a session" do
-      session[:user_id].should be_nil
-      post :create, provider: :github
-      session[:user_id].should_not be_nil
-    end
-
-    xit "should redirect the user to the root url" do
-      post :create, provider: :github
-      response.should redirect_to '/dashboard'
-    end
-
   it 'can validate user' do
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     OmniAuth.config.add_mock(:github, {:uid => '12345',
-                                       :name => 'kathleen',
                                        :extra => {:raw_info =>
-                                                 {:login => "kathleen-carroll"}},
-                                       :credentials => {:token => 'kGig93ethksIId85e'}})
+                                                  {:login => "kathleen-carroll",
+                                                   :name => 'kathleen'}
+                                                },
+                                       :credentials => {:token => ENV["GITHUB_TEST_TOKEN_2"]}})
 
     visit '/dashboard'
     click_on "Connect Github"
 
-    expect(page).to have_content('Repositories')
+    expect(page).to have_content('Repos')
+    expect(page).to have_content('adopt_dont_shop')
+    expect(page).to have_content('Followers')
+    expect(page).to have_content('kathleen-carroll')
+    expect(page).to have_content('theborg10')
+    expect(page).to have_content('Following on Github')
+    expect(page).to have_content('kathleen-carroll')
   end
 end
