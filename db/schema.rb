50e9c78cc528447deffe7ad0890d0d79d2b102de
William Meighan <williamdmeighan@gmail.com>
2020-04-01 14:19:00 -0600

    add active column to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_26_000940) do
+ActiveRecord::Schema.define(version: 2020_04_01_143853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2020_03_26_000940) do
     t.string "username"
     t.string "uid"
     t.string "token"
+    t.boolean "active?", default: false
     t.index ["email"], name: "index_users_on_email"
   end
 
